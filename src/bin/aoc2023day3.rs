fe2d8143576f4a5001e3f268f3544e1dc2dc4aa8
daltontf1212 <daltontf1212@gmail.com>
2023-12-03 21:42:25 -0600

    Some day3 "golf"

--- src/bin/aoc2023day3.rs
+++ src/bin/aoc2023day3.rs
@@ -1,16 +1,17 @@
-use std::{io, collections::VecDeque};
+use std::io; 
+
+use ::bounded_vec_deque::BoundedVecDeque;
 
 use regex::{Regex, Match};
 
 use std::io::prelude::*;
 
-fn has_number_adjacent(
-    symbols: &Vec<usize>,
-    number: &(u32, usize, usize)   
+fn has_adjacent_number(
+    symbol_indices: &Vec<usize>,
+    number_info: &(u32, usize, usize)   
 ) -> bool {
-    for symbol_index in symbols {
-        println!("number {} {} to {}, symbol at {}", number.0, number.1, number.2, symbol_index);
-        if symbol_index + 1 >= number.1 && symbol_index <= &number.2 {
+    for symbol_index in symbol_indices {
+        if symbol_index + 1 >= number_info.1 && symbol_index <= &number_info.2 {
             return true;
         }
     }
@@ -18,39 +19,31 @@ fn has_number_adjacent(
 }
 
 fn sum_adjacent_numbers(
-    row_symbols: &VecDeque<Vec<usize>>,
-    numbers:&Vec<(u32, usize, usize)>) -> u32 {
+    rows_symbol_indices: &BoundedVecDeque<Vec<usize>>,
+    number_infos:&Vec<(u32, usize, usize)>) -> u32 {
 
-    let mut result: u32 = 0;
-
-    if !numbers.is_empty() {       
-        for number in numbers {
-            let mut has_adjacency = false;
-            for symbols in row_symbols {
-                has_adjacency |= has_number_adjacent(&symbols, &number);
-            }
-            if has_adjacency {
-                println!("Number {} has adjacency", number.0);
-                result += number.0;
-            } else {
-                println!("Number {} has NO adjacency", number.0);
-            }
+    number_infos.iter().fold(0u32, |mut result, number_info| {
+        let mut has_adjacency = false;
+        for row_symbol_indices in rows_symbol_indices {
+            has_adjacency |= has_adjacent_number(&row_symbol_indices, &number_info);
+            if has_adjacency { break; } // not using symbols_in_rows..fold allows short-circuit
         }
-    }
-
-    result
+        if has_adjacency {
+            result += number_info.0;
+        }
+        result
+    })
 }
 
 fn main() -> io::Result<()> {
     let number_re = Regex::new(r"(\d+)").unwrap();
     let symbol_re = Regex::new(r"([\$#\+\*])").unwrap();
 
-    let mut row_symbols: VecDeque<Vec<usize>> = VecDeque::new();
+    let mut row_symbols: BoundedVecDeque<Vec<usize>> = BoundedVecDeque::new(3);
+    // Behave like a row of no symbols is before first row
     let mut last_numbers_row: Vec<(u32, usize, usize)> = Vec::new();
 
     let mut result: u32 = 0;
-
-    row_symbols.push_back(Vec::new()); // 
 
     for line in io::stdin().lock().lines() {
         let line_str = &line.unwrap();
@@ -61,10 +54,6 @@ fn main() -> io::Result<()> {
                 capture.start()    
             }).collect());
 
-        if row_symbols.len() > 3 {
-            row_symbols.pop_front();
-        }
-
         result += sum_adjacent_numbers(&row_symbols, &last_numbers_row);
 
         last_numbers_row = number_re.captures_iter(&line_str)
@@ -74,10 +63,7 @@ fn main() -> io::Result<()> {
             }).collect();
     }
 
-    row_symbols.push_back(Vec::new());
-    if row_symbols.len() > 3 {
-        row_symbols.pop_front();
-    } 
+    row_symbols.pop_front(); // remove third to last row symbol data and evaluate final row
 
     result += sum_adjacent_numbers(&row_symbols, &last_numbers_row);
 
